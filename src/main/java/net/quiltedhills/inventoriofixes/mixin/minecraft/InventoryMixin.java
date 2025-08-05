fa0ae8a7a66af4c139f6eb0a90878672204897e0
quiltedhills <quiltedhills@protonmail.com>
2025-08-06 02:20:50 +0500

    1.1.1 Single change :p - Make the temporary tool popup more responsive

--- src/main/java/net/quiltedhills/inventoriofixes/mixin/minecraft/InventoryMixin.java
+++ src/main/java/net/quiltedhills/inventoriofixes/mixin/minecraft/InventoryMixin.java
@@ -13,22 +13,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  * An edge case with Inventorio's displayed tools is that when you have a valid tool
  * selected in hotbar *while* there is a tool active from Inventorio's tool slots,
  * the game will correctly use the displayed tool but incorrectly use the hotbar tool's mining speed.
- * This patch checks for an active Inventorio tool to force using its tool speed instead.
+ * This patch makes the displayed tool immediately hide if the main hand tool is used.
  */
 @Mixin(Inventory.class)
 public abstract class InventoryMixin {
     @Inject(
             method = "getDestroySpeed(Lnet/minecraft/world/level/block/state/BlockState;)F",
-            at = @At("HEAD"),
-            cancellable = true
+            at = @At("HEAD")
     )
     private void patchGetDestroySpeed(BlockState state, CallbackInfoReturnable<Float> cir) {
         Inventory self = (Inventory)(Object) this;
 
         MixinHelpers.withInventoryAddon(self.player, (inventorioAddon) -> {
-            ItemStack mainHandStack = inventorioAddon.getDisplayedMainHandStack();
-            if (mainHandStack != null) {
-                cir.setReturnValue(mainHandStack.getDestroySpeed(state));
+            ItemStack preferredTool = inventorioAddon.getMostPreferredTool(state);
+            if (preferredTool == inventorioAddon.getActualMainHandItem()) {
+                inventorioAddon.setDisplayTool(ItemStack.EMPTY);
             }
         });
     }
